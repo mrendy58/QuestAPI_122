664ab4d407d8f1c176352529f8d9bb55e1ac6609
mrendy58 <derendyyy58@gmail.com>
2024-12-29 12:05:30 +0700

    membuat InsertViewModel untuk mengelola data satate form insert mahasiswa

---
+++ app/src/main/java/com/example/p8meeting12/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.p8meeting12.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.p8meeting12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+    fun updateInsertMhsState(insertUiEvent.InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
