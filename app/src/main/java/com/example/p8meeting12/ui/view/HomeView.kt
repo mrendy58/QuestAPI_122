a88d4f232a3b9dc1ece65cba1f13a770775912dc
mrendy58 <derendyyy58@gmail.com>
2024-12-29 21:12:15 +0700

    membuat object untuk menentukan rute navigasi ke halaman home

---
+++ app/src/main/java/com/example/p8meeting12/ui/view/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.p8meeting12.ui.view
+
+import com.example.p8meeting12.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
+    override val titleRes = "Home Mhs"
+}
