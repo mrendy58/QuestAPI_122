fb46d20bc74253af80f6f6a5f51166220e6883f4
mrendy58 <derendyyy58@gmail.com>
2024-12-29 19:55:30 +0700

    membuat object untuk menentukan rute navigasi ke halaman input data mahasiswa

---
+++ app/src/main/java/com/example/p8meeting12/ui/view/InsertView.kt
@@ -0,0 +1,11 @@
+package com.example.p8meeting12.ui.view
+
+import com.example.p8meeting12.ui.navigation.DestinasiNavigasi
+
+
+object DestinasiEntry : DestinasiNavigasi {
+
+    override val route = "item_entry"
+
+    override val titleRes = "Entry Mhs"
+}
