385f14533c80dcd2be77a25c57e20506b5d0b857
mrendy58 <derendyyy58@gmail.com>
2024-12-29 21:52:13 +0700

    membuat fungsi untuk menampilkan status data mahasiswa dalam layar utama

--- app/src/main/java/com/example/p8meeting12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/p8meeting12/ui/view/HomeView.kt
@@ -1,5 +1,8 @@
 package com.example.p8meeting12.ui.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -13,9 +16,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.p8meeting12.model.Mahasiswa
 import com.example.p8meeting12.ui.customwidget.CustomTopAppBar
 import com.example.p8meeting12.ui.navigation.DestinasiNavigasi
+import com.example.p8meeting12.ui.viewmodel.HomeUiState
 import com.example.p8meeting12.ui.viewmodel.HomeViewModel
 import com.example.p8meeting12.ui.viewmodel.PenyediaViewModel
 
@@ -65,5 +72,37 @@ fun HomeScree(
                 viewModel.getMhs()
             }
         )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when(homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.mahasiswa.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Mahasiswa")
+                }
+            } else {
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.nim)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
 }
