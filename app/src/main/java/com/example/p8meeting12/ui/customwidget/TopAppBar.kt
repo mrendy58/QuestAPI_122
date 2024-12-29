bff23be3d2925e73b37b6e8a39beca65d7732882
mrendy58 <derendyyy58@gmail.com>
2024-12-29 11:39:12 +0700

    membuat CenterAlignedTopAppBar header atas dengan teks yang sejajar di tengah

--- app/src/main/java/com/example/p8meeting12/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/p8meeting12/ui/customwidget/TopAppBar.kt
@@ -1,5 +1,13 @@
 package com.example.p8meeting12.ui.customwidget
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
@@ -15,5 +23,20 @@ fun CustomTopAppBar(
     navigateUp: () -> Unit = {}, // Fungsi yang dipanggil saat tombol navigasi kembali ditekan.
     onRefresh: () -> Unit = {} // Fungsi yang dipanggil saat tombol refresh ditekan.
 ){
-
+    CenterAlignedTopAppBar(
+        title = {Text(title)},
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable {
+                onRefresh()
+            })
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
+        }
+    )
 }
