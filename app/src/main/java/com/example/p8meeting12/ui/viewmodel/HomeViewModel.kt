29b9ad810b1917f7d39bd56f4999f5cba4bbe439
mrendy58 <derendyyy58@gmail.com>
2024-12-29 12:57:24 +0700

    membuat HomeViewModel untuk Mengelola status UI untuk daftar mahasiswa,Mengambil data,Menghapus data, dan Menangani kesalahan error.

--- app/src/main/java/com/example/p8meeting12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/p8meeting12/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,58 @@
 package com.example.p8meeting12.ui.viewmodel
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import kotlinx.coroutines.launch
+import java.io.IOException
 import com.example.p8meeting12.model.Mahasiswa
+import com.example.p8meeting12.repository.MahasiswaRepository
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error: HomeUiState()
     object Loading: HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+
+            mhsUiState = HomeUiState.Loading
+
+            mhsUiState = try {
+
+                HomeUiState.Success(mhs.getMahasiswa())
+            }catch (e: IOException) {
+
+                HomeUiState.Error
+            }catch (e: HttpException) {
+
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+
+            try {
+
+                mhs.deleteMahasiswa(nim)
+            }catch (e:IOException) {
+                HomeUiState.Error
+            }catch (e:HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
