8b4f9d5cedcb3c8678716cb02a537d0d6b48f1e2
mrendy58 <derendyyy58@gmail.com>
2024-12-29 12:27:25 +0700

    membuat data class untuk menyimpan data yg di inputkan

--- app/src/main/java/com/example/p8meeting12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p8meeting12/ui/viewmodel/InsertViewModel.kt
@@ -26,6 +26,15 @@ class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel() {
     }
 }
 
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
