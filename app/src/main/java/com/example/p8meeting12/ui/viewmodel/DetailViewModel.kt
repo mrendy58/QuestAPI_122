a8679c4032c62b67cbe1405a7991bfad6c976820
mrendy58 <derendyyy58@gmail.com>
2024-12-30 10:26:02 +0700

    membuat data class DetailUiState

--- app/src/main/java/com/example/p8meeting12/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/p8meeting12/ui/viewmodel/DetailViewModel.kt
@@ -2,6 +2,19 @@ package com.example.p8meeting12.ui.viewmodel
 
 import com.example.p8meeting12.model.Mahasiswa
 
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
 fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
     return InsertUiEvent(
         nim = nim,
