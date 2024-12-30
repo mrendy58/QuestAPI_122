4c8b6b325e345c4689551251b25005b1b713f1c9
mrendy58 <derendyyy58@gmail.com>
2024-12-30 10:18:42 +0700

    membuat fungsi untuk  mengubah data mahasiswa menjadi DetailUiEvent

---
+++ app/src/main/java/com/example/p8meeting12/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.p8meeting12.ui.viewmodel
+
+import com.example.p8meeting12.model.Mahasiswa
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
