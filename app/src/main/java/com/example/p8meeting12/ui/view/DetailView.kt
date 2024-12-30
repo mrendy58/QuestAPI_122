36330b0cb49a858513086cb0440b7aea257f4480
mrendy58 <derendyyy58@gmail.com>
2024-12-30 11:03:25 +0700

    membuat fungsi ComponentDetailMhs untuk menampilkan detail dalam bentuk layout

--- app/src/main/java/com/example/p8meeting12/ui/view/DetailView.kt
+++ app/src/main/java/com/example/p8meeting12/ui/view/DetailView.kt
@@ -1,5 +1,14 @@
 package com.example.p8meeting12.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.p8meeting12.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -7,4 +16,30 @@ object DestinasiDetail: DestinasiNavigasi {
     const val NIM = "nim"
     override val titleRes = "Detail Mahasiswa"
     val routeWithArg = "$route/{$NIM}"
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+    titleColor: Color = Color.Gray
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = titleColor
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
 }
